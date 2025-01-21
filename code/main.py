21b9b589a8dd5f0921cf73f22e61568f4d2da7af
Adam Zborovsky <96923903+Adam-Zborovsky@users.noreply.github.com>
2025-01-21 13:07:37 +0200

    fixed for wix use

--- code/main.py
+++ code/main.py
@@ -10,14 +10,18 @@ print("Hello Mr.Weiss")
 base_url = input("Please Enter Your Desired Url And Press Enter:  ")
 num_of_pages = int(
     input("How Many Pages Dose Your Url Have? And Press Enter:  "))
+id = int(
+    input("How Many Products Do you have in your site ? And Press Enter:  "))
 confirm = ""
 while confirm != "y":
-    confirm = input(f"Is This The Correct Url and Number Of Pages?:\n{
-                    base_url} \n {num_of_pages} \n y/n?: ")
+    confirm = input(f"Is This The Correct Url and Number Of Pages and Number Of products?:\n{base_url} \n {num_of_pages} \n {id} \n y/n?: ")
     if confirm != "y":
         base_url = input("Please Enter Your Desired Url And Press Enter:")
         num_of_pages = int(
             input("How Many Pages Dose Your Url Have? And Press Enter:  "))
+        id = int(
+            input("How Many Products Do you have in your site ? And Press Enter:  "))
+
 
 
 shutil.rmtree("Result", ignore_errors=True)
@@ -40,7 +44,7 @@ for i in range(1, num_of_pages + 1):
 
 product_data = []
 dupes = 1
-for i, url in enumerate(urls):
+for url in urls:
     name = ""
     price = ""
     content = []
@@ -48,37 +52,36 @@ for i, url in enumerate(urls):
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "lxml")
 
-    # print(f"downloading prod_{i}")
 
     h1_tags = soup.find_all("h1", class_="product_title entry-title h2")
     for h1_tag in h1_tags:
         name = h1_tag.text.strip()
 
-    active_li_tag = soup.find("li", class_="active")
-    if active_li_tag:
-        content_div_tags = active_li_tag.find_all("div", class_="tab-content")
-        for content_div_tag in content_div_tags:
-            for child in content_div_tag.find_all(["p", "ul", "li", "h3"]):
-                text = child.text.strip()
-                if text:
-                    text = text.replace(",", ".")
-                    content.append(text)
-    else:
-        content_div_tags = soup.find_all("div", class_="tab-content")
-        for content_div_tag in content_div_tags:
-            for child in content_div_tag.find_all("p"):
-                text = child.text.strip()
-                if text:
-                    text = text.replace(",", ".")
-                    content.append(text)
+    # active_li_tag = soup.find("li", class_="active")
+    # if active_li_tag:
+    #     content_div_tags = active_li_tag.find_all("div", class_="tab-content")
+    #     for content_div_tag in content_div_tags:
+    #         for child in content_div_tag.find_all(["p", "ul", "li", "h3"]):
+    #             text = child.text.strip()
+    #             if text:
+    #                 text = text.replace(",", ".")
+    #                 content.append(text)
+    # else:
+    #     content_div_tags = soup.find_all("div", class_="tab-content")
+    #     for content_div_tag in content_div_tags:
+    #         for child in content_div_tag.find_all("p"):
+    #             text = child.text.strip()
+    #             if text:
+    #                 text = text.replace(",", ".")
+    #                 content.append(text)
 
-    description_div_tags = soup.find_all("div", class_="row description")
+    # description_div_tags = soup.find_all("div", class_="row description")
 
-    for description_div_tag in description_div_tags:
-        description_p = description_div_tag.find("p")
-        if description_p:
-            text = description_p.text.strip()
-            description.append(text)
+    # for description_div_tag in description_div_tags:
+    #     description_p = description_div_tag.find("p")
+    #     if description_p:
+    #         text = description_p.text.strip()
+    #         description.append(text)
 
     price_p_tags = soup.find_all("p", class_="price prm-color")
 
@@ -93,16 +96,36 @@ for i, url in enumerate(urls):
     name = name[:80]
 
     product_data.append(
-        [f"Product_{i}", "Product", name, price, ' '.join(description), ' '.join(content)])
+        [f"Product_{id}", "Product", name, price])
 
-    print(f"Downloading Product_{i}")
-
+    print(f"Downloading Product_{id}")
+    id += 1
+    
     product_images = []
-    img_tags = soup.find_all("img", class_="wp-post-image")
-    for img_tag in img_tags[1:]:
-        src = img_tag.get("data-large_image")
-        if src:
-            product_images.append(src)
+
+    div_class_regex = re.compile(r"woocommerce-product-gallery__image(\s|\.current)?")
+
+    gallery_divs = soup.find_all("div", class_=div_class_regex)
+
+    for div in gallery_divs:
+        a_tag = div.find("a")
+        if a_tag:
+            img_tag = a_tag.find("img")
+            img_src = img_tag.get("data-large_image")
+            if img_tag and img_src:
+                product_images.append(img_src)
+                print(img_src)
+            else:
+                picture_tag = a_tag.find("picture")
+                if picture_tag:
+                    img_tag_in_picture = picture_tag.find("img")
+                    picture_src = img_tag_in_picture.get("data-large_image")
+                    if img_tag_in_picture and picture_src:
+                        product_images.append(picture_src)
+                        print(picture_src)
+
+
+
 
     base_path = f"Result/{name}"
     if os.path.exists(base_path):
@@ -111,10 +134,9 @@ for i, url in enumerate(urls):
     else:
         os.mkdir(base_path)
 
-    for j, img_url in enumerate(product_images):
-        # Sanitize product name for use as a file name
+    for i, img_url in enumerate(product_images):
 
-        img_filename = f"{base_path}/{j+1}.jpg"
+        img_filename = f"{base_path}/{i+1}.jpg"
         try:
             img_data = requests.get(img_url).content
             with open(img_filename, 'wb') as img_file:
